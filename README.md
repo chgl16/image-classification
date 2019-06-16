6a7a4fea7341c0ceaf2984dde5dcaaf452a192c6
chgl16 <chenbetter1996@gmail.com>
2019-06-16 16:43:51 +0800

    add: 数据集处理

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 基于CNN的海贼王人物图像多分类
-1. [根据关键字爬取数据集](code/data-crawling/README.md)
+1. [根据关键字爬取数据集](code/data-crawling/)
+2. [数据集处理](code/data-processing/)
     
---
+++ code/data-processing/batch_rename.py
@@ -0,0 +1,12 @@
+﻿import os
+
+# 需要批量改名的文件所在文件夹
+path_name='../../data/raw/乔巴/'
+
+# 命名从1开始
+i = 1
+for item in os.listdir(path_name):
+    # 进入到文件夹内，对每个文件进行循环遍历
+    # os.path.join(path_name, item)表示找到每个文件的绝对路径并进行拼接操作
+    os.rename(os.path.join(path_name, item), os.path.join(path_name, (str(i) + '.png')))
+    i += 1
